be15bc604c645383ed005693f1e1ec5d656b724c
thangpn <thangpn@iceteasoftware.com>
2025-04-10 17:55:07 +0700

    icon

---
+++ lib/core/locale/generated/intl/messages_vi_VN.dart
@@ -0,0 +1,35 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a vi_VN locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'vi_VN';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "addActivity": MessageLookupByLibrary.simpleMessage("Thêm hoạt động"),
+    "clearCache": MessageLookupByLibrary.simpleMessage("Xóa bộ nhớ đệm"),
+    "darkMode": MessageLookupByLibrary.simpleMessage("Chế độ tối"),
+    "dashboard": MessageLookupByLibrary.simpleMessage("Tổng quan"),
+    "expenses": MessageLookupByLibrary.simpleMessage("Chi phí"),
+    "income": MessageLookupByLibrary.simpleMessage("Thu nhập"),
+    "language": MessageLookupByLibrary.simpleMessage("Ngôn ngữ"),
+    "settings": MessageLookupByLibrary.simpleMessage("Cài đặt"),
+    "totalBalance": MessageLookupByLibrary.simpleMessage("Tổng số dư"),
+  };
+}
