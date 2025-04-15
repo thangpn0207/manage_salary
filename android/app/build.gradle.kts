c1c689a02d6d2a041d527ce958b016c1b9814439
thangpn <thangpn0207@gmail.com>
2025-04-15 20:30:57 +0700

    feat: Update Android build configuration with specific SDK versions and dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,6 +4,7 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -15,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "id.thangpn.manage_salary"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -66,8 +67,9 @@ android {
     }
 
     defaultConfig {                                              
-        minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        multiDexEnabled=true
+        minSdk=23
+        targetSdk=34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -103,4 +105,6 @@ flutter {
 
 dependencies {
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.gms:play-services-ads:24.2.0")
+    implementation("androidx.multidex:multidex:2.0.1")
 }
